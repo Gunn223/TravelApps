7f98d0a7605050ecdd40fdae6fa76e99a34b5553
Gunn223 <pamungkasguntur40@gmail.com>
2024-01-01 11:47:58 +0700

    create: app yaml

--- app/server.js
+++ app/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const methodOverride = require('method-override');
-const port = 9000;
+const port = process.env.PORT || 9000;
+app.set('trust proxy', true);
 const cors = require('cors');
 
 // override with the X-HTTP-Method-Override header in the request
@@ -19,7 +21,7 @@ app.use('/users', require('../Routes/User/userRoutes'));
 app.use('/destination', require('../Routes/Destination/destinationRoutes'));
 app.use('/booking', require('../Routes/Booking/bookingroutes'));
 
-const server = app.listen(port, () => {
+const server = app.listen(port, 3000, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
